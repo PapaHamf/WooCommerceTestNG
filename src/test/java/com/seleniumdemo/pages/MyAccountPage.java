04c0fc32bb33e4ce2e7c37c6d89fd43c246e4093
PapaHamf <jaroslaw.puchala@gmail.com>
2025-08-06 20:56:25 +0200

    Added register user test

--- src/test/java/com/seleniumdemo/pages/MyAccountPage.java
+++ src/test/java/com/seleniumdemo/pages/MyAccountPage.java
@@ -1,14 +1,32 @@
 package com.seleniumdemo.pages;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.FindBys;
 import org.openqa.selenium.support.PageFactory;
+
+import java.util.List;
 
 public class MyAccountPage {
 
     protected WebDriver driver;
 
+    @FindBys({@FindBy(className = "woocommerce-MyAccount-content"), @FindBy(tagName = "p")})
+    private List<WebElement> welcomeText;
+    @FindBy(className = "entry-title")
+    private WebElement myAccountHeading;
+
     public MyAccountPage(WebDriver driver) {
         PageFactory.initElements(driver, this);
         this.driver = driver;
+    }
+
+    public String getWelcomeText() {
+        return welcomeText.get(1).getText();
+    }
+
+    public String getMyAccountHeading() {
+        return myAccountHeading.getText();
     }
 }
--- src/test/java/com/seleniumdemo/tests/RegisterTest.java
+++ src/test/java/com/seleniumdemo/tests/RegisterTest.java
@@ -1,15 +1,35 @@
 package com.seleniumdemo.tests;
 
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.Status;
 import com.seleniumdemo.pages.HomePage;
+import com.seleniumdemo.pages.MyAccountPage;
 import com.seleniumdemo.pages.RegisterUserPage;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class RegisterTest extends BaseTest {
 
-    @Test
+    //sprawdzić, jak zapisywać do pliku
+    private static final Logger logger = LogManager.getLogger();
+
+    @Test(enabled = false)
     public void registerUserTest() {
+        ExtentTest test = extentReports.createTest("Register User Test");
         HomePage homePage = new HomePage(driver);
         RegisterUserPage registerUserPage = homePage.clickMyAccount();
+        String email = "mareczek@testowy.pl";
+        logger.info("Entering email address: " + email);
+        test.log(Status.PASS, "Entering email address: " + email);
+        registerUserPage.enterRegisterEmail(email);
+        String password = "testowy123";
+        logger.info("Entering password: " + password);
+        test.log(Status.PASS, "Entering password: " + password);
+        registerUserPage.enterRegisterPassword(password);
+        MyAccountPage myAccountPage = registerUserPage.clickRegisterButton();
+        Assert.assertEquals(myAccountPage.getMyAccountHeading(), "My account");
         try {
             Thread.sleep(3000);
         } catch (InterruptedException e) {
