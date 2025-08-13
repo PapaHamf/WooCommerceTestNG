2b8ae763511e5f1dccd57b39195778a992e13381
PapaHamf <jaroslaw.puchala@gmail.com>
2025-08-13 13:53:08 +0200

    Added Product Page object

---
+++ src/test/java/com/seleniumdemo/pages/ProductPage.java
@@ -0,0 +1,55 @@
+package com.seleniumdemo.pages;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.FindBys;
+import org.openqa.selenium.support.PageFactory;
+
+public class ProductPage {
+
+    protected WebDriver driver;
+
+    @FindBy(name = "add-to-cart")
+    private WebElement addToCartButton;
+    @FindBy(className = "entry-summary")
+    private WebElement productEntry;
+    @FindBys({@FindBy(className = "entry-summary"), @FindBy(className = "amount")})
+    private WebElement productPrice;
+    @FindBy(id = "tab-title-description")
+    private WebElement descriptionLink;
+    @FindBy(id = "tab-title-reviews")
+    private WebElement reviewsLink;
+    @FindBy(id = "tab-description")
+    private WebElement productDescriptionContainer;
+    @FindBys({@FindBy(id = "tab-description"), @FindBy(tagName = "p")})
+    private WebElement productDescription;
+
+    public ProductPage(WebDriver driver) {
+        PageFactory.initElements(driver, this);
+        this.driver = driver;
+    }
+
+    public ProductPage clickAddToCart() {
+        addToCartButton.click();
+        return this;
+    }
+
+    public String getProductPrice() {
+        return productPrice.getText();
+    }
+
+    public ProductPage switchToDescription() {
+        descriptionLink.click();
+        return this;
+    }
+
+    public ProductPage switchToReviews() {
+        reviewsLink.click();
+        return this;
+    }
+
+    public String getProductDescription() {
+        return productDescription.getText();
+    }
+}
