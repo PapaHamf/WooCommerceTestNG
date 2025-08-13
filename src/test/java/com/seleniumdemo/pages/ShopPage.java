f5d33c03314fb128273d35320f27c12c82e00f25
PapaHamf <jaroslaw.puchala@gmail.com>
2025-08-13 14:33:17 +0200

    Added Cart widget

--- src/test/java/com/seleniumdemo/pages/ShopPage.java
+++ src/test/java/com/seleniumdemo/pages/ShopPage.java
@@ -2,12 +2,14 @@ package com.seleniumdemo.pages;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.FindBys;
 import org.openqa.selenium.support.PageFactory;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class ShopPage {
 
@@ -21,6 +23,15 @@ public class ShopPage {
     private List<WebElement> productButtons;
     @FindBys({@FindBy(className = "products"), @FindBy(tagName = "h2")})
     private List<WebElement> productNames;
+    // Cart Widget
+    @FindBy(className = "icn-shoppingcart")
+    private List<WebElement> cartIcon;
+    @FindBy(className = "total")
+    private WebElement productsTotal;
+    @FindBy(linkText = "View cart")
+    private WebElement viewCartButton;
+    @FindBy(linkText = "Checkout")
+    private WebElement checkOutButton;
 
     public ShopPage(WebDriver driver) {
         PageFactory.initElements(driver, this);
@@ -32,11 +43,10 @@ public class ShopPage {
     }
 
     public List<String> getProductNames() {
-        List<String> names = new ArrayList<String>();
-        for ( WebElement element: productNames ) {
-            names.add(element.getText());
-        }
-        return names;
+        return productNames
+                .stream()
+                .map(WebElement::getText)
+                .collect(Collectors.toList());
     }
 
     public List<Integer> getProductPrices() {
@@ -62,4 +72,40 @@ public class ShopPage {
         return this;
     }
 
+    public ShopPage clickCartIcon() {
+        cartIcon
+                .stream()
+                .filter(WebElement::isDisplayed)
+                .findFirst()
+                .ifPresent(WebElement::click);
+        return this;
+    }
+
+    public WebElement getInteractiveCartIcon() {
+        return cartIcon
+                .stream()
+                .filter(WebElement::isDisplayed)
+                .findFirst()
+                .get();
+    }
+
+    public ShopPage displayCartWidget() {
+        Actions action = new Actions(driver);
+        action.moveToElement(getInteractiveCartIcon()).perform();
+        return this;
+    }
+
+    public String getCartWidgetTotal() {
+        return productsTotal.getText();
+    }
+
+    public CartPage clickViewCartButton() {
+        viewCartButton.click();
+        return new CartPage(driver);
+    }
+
+    public CheckOutPage clickCheckoutButton() {
+        checkOutButton.click();
+        return new CheckOutPage(driver);
+    }
 }
