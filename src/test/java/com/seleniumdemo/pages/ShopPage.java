6bd0c5128d986920d7136c7e7c45c2300a45fed9
PapaHamf <jaroslaw.puchala@gmail.com>
2025-08-14 13:47:12 +0200

    Moved Cart widget to HomePage

--- src/test/java/com/seleniumdemo/pages/ShopPage.java
+++ src/test/java/com/seleniumdemo/pages/ShopPage.java
@@ -2,10 +2,10 @@ package com.seleniumdemo.pages;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.FindBys;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.Select;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -23,15 +23,8 @@ public class ShopPage {
     private List<WebElement> productButtons;
     @FindBys({@FindBy(className = "products"), @FindBy(tagName = "h2")})
     private List<WebElement> productNames;
-    // Cart Widget
-    @FindBy(className = "icn-shoppingcart")
-    private List<WebElement> cartIcon;
-    @FindBy(className = "total")
-    private WebElement productsTotal;
-    @FindBy(linkText = "View cart")
-    private WebElement viewCartButton;
-    @FindBy(linkText = "Checkout")
-    private WebElement checkOutButton;
+    @FindBy(name = "orderby")
+    private WebElement sortOrderList;
 
     public ShopPage(WebDriver driver) {
         PageFactory.initElements(driver, this);
@@ -72,40 +65,14 @@ public class ShopPage {
         return this;
     }
 
-    public ShopPage clickCartIcon() {
-        cartIcon
-                .stream()
-                .filter(WebElement::isDisplayed)
-                .findFirst()
-                .ifPresent(WebElement::click);
-        return this;
+    public void selectProductOrderByIndex(int index) {
+        Select select = new Select(sortOrderList);
+        select.selectByIndex(index);
     }
 
-    public WebElement getInteractiveCartIcon() {
-        return cartIcon
-                .stream()
-                .filter(WebElement::isDisplayed)
-                .findFirst()
-                .get();
+    public void selectProductOrderByValue(String name) {
+        Select select = new Select(sortOrderList);
+        select.selectByValue(name);
     }
 
-    public ShopPage displayCartWidget() {
-        Actions action = new Actions(driver);
-        action.moveToElement(getInteractiveCartIcon()).perform();
-        return this;
-    }
-
-    public String getCartWidgetTotal() {
-        return productsTotal.getText();
-    }
-
-    public CartPage clickViewCartButton() {
-        viewCartButton.click();
-        return new CartPage(driver);
-    }
-
-    public CheckOutPage clickCheckoutButton() {
-        checkOutButton.click();
-        return new CheckOutPage(driver);
-    }
 }
