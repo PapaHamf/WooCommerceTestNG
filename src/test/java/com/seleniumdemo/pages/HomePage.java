9cba0ea040a7e7fe89ba094e6fd14823c33e68ee
PapaHamf <jaroslaw.puchala@gmail.com>
2025-08-14 13:46:50 +0200

    Moved Cart widget to HomePage

--- src/test/java/com/seleniumdemo/pages/HomePage.java
+++ src/test/java/com/seleniumdemo/pages/HomePage.java
@@ -2,8 +2,11 @@ package com.seleniumdemo.pages;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+
+import java.util.List;
 
 public class HomePage {
 
@@ -15,8 +18,20 @@ public class HomePage {
     @FindBy(xpath = "//span[contains(text(), 'Cart')]")
     private WebElement cartLink;
     @FindBy(xpath = "//span[contains(text(), 'Shop')]")
-
     private WebElement shopLink;
+    @FindBy(className = "nav__search")
+    private List<WebElement> searchBarButton;
+    // Cart Widget Elements
+    @FindBy(className = "icn-shoppingcart")
+    private List<WebElement> cartIcon;
+    @FindBy(className = "total")
+    private WebElement productsTotal;
+    @FindBy(linkText = "View cart")
+    private WebElement viewCartButton;
+    @FindBy(linkText = "Checkout")
+    private WebElement checkOutButton;
+    // dodać remove button
+    // remove_from_cart_button class
 
     public HomePage(WebDriver driver) {
         PageFactory.initElements(driver, this);
@@ -37,4 +52,51 @@ public class HomePage {
         shopLink.click();
         return new ShopPage(driver);
     }
+
+    public HomePage clickSearchButton() {
+        searchBarButton
+                .stream()
+                .filter(WebElement::isDisplayed)
+                .findFirst()
+                .ifPresent(WebElement::click);
+        return this;
+    }
+
+    public HomePage clickCartIcon() {
+        cartIcon
+                .stream()
+                .filter(WebElement::isDisplayed)
+                .findFirst()
+                .ifPresent(WebElement::click);
+        return this;
+    }
+
+    public WebElement getInteractiveCartIcon() {
+        return cartIcon
+                .stream()
+                .filter(WebElement::isDisplayed)
+                .findFirst()
+                .get();
+    }
+
+    public HomePage displayCartWidget() {
+        Actions action = new Actions(driver);
+        action.moveToElement(getInteractiveCartIcon()).perform();
+        return this;
+    }
+
+    public String getCartWidgetTotal() {
+        return productsTotal.getText();
+    }
+
+    public CartPage clickViewCartButton() {
+        viewCartButton.click();
+        return new CartPage(driver);
+    }
+
+    public CheckOutPage clickCheckoutButton() {
+        checkOutButton.click();
+        return new CheckOutPage(driver);
+    }
+
 }
