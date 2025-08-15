453c048b8a6fabeae44f8ff02e8d9d87183aac27
PapaHamf <jaroslaw.puchala@gmail.com>
2025-08-15 19:20:23 +0200

    Finished the checkout test

--- src/test/java/com/seleniumdemo/tests/ShopTest.java
+++ src/test/java/com/seleniumdemo/tests/ShopTest.java
@@ -2,10 +2,11 @@ package com.seleniumdemo.tests;
 
 import com.aventstack.extentreports.ExtentTest;
 import com.aventstack.extentreports.Status;
-import com.seleniumdemo.pages.HomePage;
-import com.seleniumdemo.pages.ShopPage;
+import com.seleniumdemo.models.Customer;
+import com.seleniumdemo.pages.*;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import java.util.Random;
@@ -15,8 +16,9 @@ public class ShopTest extends BaseTest {
     private static final Logger logger = LogManager.getLogger();
 
     @Test()
-    public void addProductToCartTest() {
+    public void checkoutTest() {
         ExtentTest test = extentReports.createTest("Add product to cart test");
+        Customer customer = new Customer();
         HomePage homePage = new HomePage(driver);
         logger.info("Entering the shop page");
         test.log(Status.PASS, "Entering the shop page");
@@ -25,6 +27,22 @@ public class ShopTest extends BaseTest {
         Random random = new Random();
         int randomNum = random.nextInt(3);
         shopPage.clickProductButtonNumber(randomNum);
+        shopPage.clickProductButtonNumber(0);
+        homePage.displayCartWidget();
+        CartPage cartPage = homePage.clickViewCartButton();
+        CheckOutPage checkOutPage = cartPage.clickCheckoutButton();
+        checkOutPage.setFirstName(customer.getFirstName());
+        checkOutPage.setLastName(customer.getLastName());
+        checkOutPage.setCompanyName(customer.getCompanyName());
+        checkOutPage.selectCountryByName(customer.getCountry());
+        checkOutPage.setStreetAddress(customer.getStreet());
+        checkOutPage.setApartmentNumber(customer.getHouseNumber());
+        checkOutPage.setPostCode(customer.getPostCode());
+        checkOutPage.setPhoneNumber(customer.getPhoneNumber());
+        checkOutPage.setCity(customer.getCity());
+        checkOutPage.setEmailAddress(customer.getEmail());
+        OrderSummaryPage orderSummaryPage = checkOutPage.clickPlaceOrder();
+        Assert.assertEquals(orderSummaryPage.getOrderNotice(), "Thank you. Your order has been received.");
         try {
             Thread.sleep(3000);
         } catch (InterruptedException e) {
