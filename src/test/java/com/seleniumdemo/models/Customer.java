cac60e988b3d920377514f27b7ef93cdde87fba9
PapaHamf <jaroslaw.puchala@gmail.com>
2025-08-15 19:20:44 +0200

    Customer class

---
+++ src/test/java/com/seleniumdemo/models/Customer.java
@@ -0,0 +1,95 @@
+package com.seleniumdemo.models;
+
+public class Customer {
+
+    private String firstName = "Mareczek";
+    private String lastName = "Testowy";
+    private String companyName = "Test organization";
+    private String country = "Germany";
+    private String street = "Pieczarkowa 1";
+    private String houseNumber = "3";
+    private String postCode = "55-500";
+    private String city = "Pcim Dolny";
+    private String phoneNumber = "12 555 9999";
+    private String email = "mareczek@testowy.pl";
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getCompanyName() {
+        return companyName;
+    }
+
+    public void setCompanyName(String companyName) {
+        this.companyName = companyName;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public String getHouseNumber() {
+        return houseNumber;
+    }
+
+    public void setHouseNumber(String houseNumber) {
+        this.houseNumber = houseNumber;
+    }
+
+    public String getPostCode() {
+        return postCode;
+    }
+
+    public void setPostCode(String postCode) {
+        this.postCode = postCode;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
