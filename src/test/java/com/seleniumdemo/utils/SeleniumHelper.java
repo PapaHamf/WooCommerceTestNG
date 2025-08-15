672268278513d6b69259b89a32ed253ba1d40d23
PapaHamf <jaroslaw.puchala@gmail.com>
2025-08-15 19:20:09 +0200

    Added ElementToBeClickable

---
+++ src/test/java/com/seleniumdemo/utils/SeleniumHelper.java
@@ -0,0 +1,32 @@
+package com.seleniumdemo.utils;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+public class SeleniumHelper {
+    public static void waitForElementToExist(WebDriver driver, By locator) {
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofMillis(5000));
+        wait.until(ExpectedConditions.presenceOfElementLocated(locator));
+    }
+
+    public static void waitForElementToBeVisible(WebDriver driver, WebElement element) {
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofMillis(5000));
+        wait.until(ExpectedConditions.visibilityOf(element));
+    }
+
+    public static void waitForList(WebDriver driver, By locator) {
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofMillis(5000));
+        wait.until(browser -> driver.findElements(locator).size() > 0);
+    }
+
+    public static void waitForElementToBeClickable(WebDriver driver, WebElement element) {
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofMillis(5000));
+        wait.until(ExpectedConditions.elementToBeClickable(element));
+    }
+
+}
