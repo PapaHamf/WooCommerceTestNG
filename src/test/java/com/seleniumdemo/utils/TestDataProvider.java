86344048a0823414dc475d0d373ff1c54b39a12b
PapaHamf <jaroslaw.puchala@gmail.com>
2025-08-22 17:29:26 +0200

    Data Provider class

---
+++ src/test/java/com/seleniumdemo/utils/TestDataProvider.java
@@ -0,0 +1,60 @@
+package com.seleniumdemo.utils;
+
+import com.github.javafaker.Faker;
+import com.opencsv.CSVReader;
+import com.opencsv.CSVReaderBuilder;
+import com.opencsv.exceptions.CsvException;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.List;
+import java.util.Random;
+
+public class TestDataProvider {
+
+    private Faker faker;
+    private Random random;
+
+    public TestDataProvider() {
+        faker = new Faker();
+        random = new Random();
+    }
+
+    public static Object[][] readExcel(String fileName) throws IOException {
+        File file = new File("src/test/resources/" + fileName);
+        FileInputStream inputStream = new FileInputStream(file);
+        Workbook workbook = null;
+        String fileExt = fileName.substring(fileName.indexOf("."));
+        if (fileExt.equals(".xlsx")) {
+            workbook = new XSSFWorkbook(inputStream);
+        } else if ( fileExt.equals(".xls")) {
+            workbook = new HSSFWorkbook(inputStream);
+        }
+        Sheet sheet = workbook.getSheetAt(0);
+        int rowNumber = sheet.getLastRowNum();
+        // fetching the column number
+        int columnNumber = sheet.getRow(0).getLastCellNum();
+        // array used to store the data
+        Object[][] data = new Object[rowNumber][columnNumber];
+        for (int i = 1; i <= rowNumber; i++) {
+            Row row = sheet.getRow(i);
+            for (int j = 0; j < columnNumber; j++) {
+                data[i-1][j] = row.getCell(j).getStringCellValue();
+            }
+        }
+        return data;
+    }
+
+    public static List<String[]> readCSV(String fileName) throws CsvException, IOException {
+        CSVReader reader = new CSVReaderBuilder(new FileReader(fileName)).build();
+        List<String[]> myEntries = reader.readAll();
+        return myEntries;
+    }
+}
