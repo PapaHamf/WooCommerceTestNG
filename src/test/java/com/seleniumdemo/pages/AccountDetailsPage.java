2ff63f3af9ce8724e438a7af77c72b4cabaf1796
PapaHamf <jaroslaw.puchala@gmail.com>
2025-11-02 22:02:21 +0100

    Account Details Page docstrings

--- src/test/java/com/seleniumdemo/pages/AccountDetailsPage.java
+++ src/test/java/com/seleniumdemo/pages/AccountDetailsPage.java
@@ -26,39 +26,75 @@ public class AccountDetailsPage {
     @FindBy(name = "save_account_details")
     private WebElement saveChangesButton;
 
+    /**
+     * Class that holds th locators of the Account Details page and methods to get its webelements.
+     * @param driver
+     */
     public AccountDetailsPage(WebDriver driver) {
         PageFactory.initElements(driver, this);
         this.driver = driver;
     }
 
+    /**
+     * Sets the first name field on the page.
+     * @param firstName Text describing the first name.
+     */
     public void setFirstName(String firstName) {
         this.firstName.sendKeys(firstName);
     }
 
+    /**
+     * Sets the last name field on the page.
+     * @param lastName Text describing the last name.
+     */
     public void setLastName(String lastName) {
         this.lastName.sendKeys(lastName);
     }
 
+    /**
+     * Sets the display name field on the page.
+     * @param displayName Text describing the display name of the user.
+     */
     public void setDisplayName(String displayName) {
         this.displayName.sendKeys(displayName);
     }
 
+    /**
+     * Sets the email address field on the page.
+     * @param emailAddress Text describing the email address of the user.
+     */
     public void setEmailAddress(String emailAddress) {
         this.emailAddress.sendKeys(emailAddress);
     }
 
+    /**
+     * Sets the current password field on the page.
+     * @param currentPassword Text containing the current password of the user.
+     */
     public void setCurrentPassword(String currentPassword) {
         this.currentPassword.sendKeys(currentPassword);
     }
 
+    /**
+     * Sets the new password field on the page.
+     * @param newPassword Text containing the new password of the user.
+     */
     public void setNewPassword(String newPassword) {
         this.newPassword.sendKeys(newPassword);
     }
 
+    /**
+     * Sets the confirm password field on the page.
+     * @param confirmPassword Text containing the confirm password of the user.
+     */
     public void setConfirmPassword(String confirmPassword) {
         this.confirmPassword.sendKeys(confirmPassword);
     }
 
+    /**
+     * Clicks the Save Changes button.
+     * @return The AccountDetails object.
+     */
     public AccountDetailsPage clickSaveChanges() {
         saveChangesButton.click();
         return this;
