9acf986c6febcd25b8cbe7fd227b3497cac1f5f2
PapaHamf <jaroslaw.puchala@gmail.com>
2025-11-04 21:07:15 +0100

    Added docstrings

--- src/test/java/com/seleniumdemo/pages/CheckOutPage.java
+++ src/test/java/com/seleniumdemo/pages/CheckOutPage.java
@@ -64,73 +64,141 @@ public class CheckOutPage {
     // Javascripts
     private static final String SCROLL_TO_ORDER_BUTTON_JS = "arguments[0].scrollIntoView(true);";
 
+    /**
+     * Class that holds the locators of the Checkout page and methods to get its webelements.
+     * @param driver
+     */
     public CheckOutPage(WebDriver driver) {
         PageFactory.initElements(driver, this);
         this.driver = driver;
     }
 
+    /**
+     * Sets the first name field on the page.
+     * @param firstName Text containing the first name
+     */
     public void setFirstName(String firstName) {
         this.firstName.sendKeys(firstName);
     }
 
+    /**
+     * Sets the last name field on the page.
+     * @param lastName Text containing the last name.
+     */
     public void setLastName(String lastName) {
         this.lastName.sendKeys(lastName);
     }
 
+    /**
+     * Sets the copmany name field on the page.
+     * @param companyName Text containing the company name of the user.
+     */
     public void setCompanyName(String companyName) {
         this.companyName.sendKeys(companyName);
     }
 
+    /**
+     * Selects the country using the index of the country list on the page.
+     * @param index Index number of the country list.
+     */
     public void selectCountryByIndex(int index) {
         Select select = new Select(selectCountry);
         select.selectByIndex(index);
     }
 
+    /**
+     * Selects the country using the name of the country list on the page.
+     * @param countryName Country name of the country list.
+     */
     public void selectCountryByName(String countryName) {
         Select select = new Select(selectCountry);
         select.selectByVisibleText(countryName);
     }
 
+    /**
+     * Sets the order comments field on the page.
+     * @param comments Text containing the order comments.
+     */
     public void setOrderComments(String comments) {
         orderComments.sendKeys(comments);
     }
 
+    /**
+     * Sets the street address field on the page.
+     * @param streetAddress Text containing the street address.
+     */
     public void setStreetAddress(String streetAddress) {
         this.streetAddress.sendKeys(streetAddress);
     }
 
+    /**
+     * Sets the apartment number field on the page.
+     * @param apartmentNumber Text containing the apartment number.
+     */
     public void setApartmentNumber(String apartmentNumber) {
         this.apartmentNumber.sendKeys(apartmentNumber);
     }
 
+    /**
+     * Sets the post code field on the page.
+     * @param postCode Text containing the post code.
+     */
     public void setPostCode(String postCode) {
         this.postCode.sendKeys(postCode);
     }
 
+    /**
+     * Sets the city field on the page.
+     * @param city Text containing the city.
+     */
     public void setCity(String city) {
         this.city.sendKeys(city);
     }
 
+    /**
+     * Sets the phone number field on the page.
+     * @param phoneNumber Text containing the phone number.
+     */
     public void setPhoneNumber(String phoneNumber) {
         this.phoneNumber.sendKeys(phoneNumber);
     }
 
+    /**
+     * Sets the email address field on the page.
+     * @param email Text containing the email address.
+     */
     public void setEmailAddress(String email) {
         emailAddress.sendKeys(email);
     }
 
+    /**
+     * Returns the table w/ the products list.
+     * @return Table webelement.
+     */
     public WebElement getTable() {
         return driver.findElement(table);
     }
 
+    /**
+     * Returns the table body /w the products list.
+     * @return Table body webelement.
+     */
     public WebElement getTableBody() {
         return getTable().findElement(tableBody);
     }
 
+    /**
+     * Returns the product list of the order.
+     * @return List of products webelements.
+     */
     public List<WebElement> getProductsList() {
         return productsList;
     }
 
+    /**
+     * Returns the list of the product names.
+     * @return List of products names.
+     */
     public List<String> getProductNames() {
         List<String> names = new ArrayList<String>();
         SeleniumHelper.waitForElementToExist(driver, tableBody);
@@ -141,6 +209,10 @@ public class CheckOutPage {
         return names;
     }
 
+    /**
+     * Returns the list of product totals.
+     * @return List of product totals.
+     */
     public List<Double> getProductTotals() {
         List<Double> totals = new ArrayList<Double>();
         SeleniumHelper.waitForElementToExist(driver, tableBody);
@@ -153,6 +225,10 @@ public class CheckOutPage {
         return totals;
     }
 
+    /**
+     * Returns the order Subtotal field value.
+     * @return Order subtotal value.
+     */
     public Double getOrderSubtotal() {
         String subtotal = orderSubtotal.findElement(By.className("amount")).getText();
         return Double.parseDouble(subtotal.substring(0, subtotal
@@ -160,6 +236,10 @@ public class CheckOutPage {
                 .replace(",", "."));
     }
 
+    /**
+     * Returns the order Total field value.
+     * @return Order total value.
+     */
     public Double getOrderTotal() {
         String total = orderTotal.findElement(By.className("amount")).getText();
         return Double.parseDouble(total.substring(0, total
@@ -167,9 +247,13 @@ public class CheckOutPage {
                 .replace(",", "."));
     }
 
+    /**
+     * Clicks the Place order button on the page.
+     * @return Order Summary page object.
+     */
     public OrderSummaryPage clickPlaceOrder() {
         JavascriptExecutor js = (JavascriptExecutor) driver;
-        js.executeScript(SCROLL_TO_ORDER_BUTTON, driver.findElement(placeOrderBtn));
+        js.executeScript(SCROLL_TO_ORDER_BUTTON_JS, driver.findElement(placeOrderBtn));
         SeleniumHelper.waitForElementToBeClickable(driver, driver.findElement(placeOrderBtn));
         driver.findElement(placeOrderBtn).click();
         return new OrderSummaryPage(driver);
