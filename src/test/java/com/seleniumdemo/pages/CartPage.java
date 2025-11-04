4541a32e13febd682e183f17ce209a6238e88029
PapaHamf <jaroslaw.puchala@gmail.com>
2025-11-04 20:47:39 +0100

    Added docstrings

--- src/test/java/com/seleniumdemo/pages/CartPage.java
+++ src/test/java/com/seleniumdemo/pages/CartPage.java
@@ -50,15 +50,27 @@ public class CartPage {
     private By productQuantity = By.xpath("//td[@data-title='Quantity']");
     private By productTotal = By.xpath("//td[@data-title='Total']");
 
+    /**
+     * Class that holds the locators of the Cart page and methods to get its webelements.
+     * @param driver
+     */
     public CartPage(WebDriver driver) {
         PageFactory.initElements(driver, this);
         this.driver = driver;
     }
 
+    /**
+     * Clicks the Remove button of the product selected by index.
+     * @param index Number of the product on the list, starts w/ 0.
+     */
     public void removeProductByNumber(int index) {
         removeButtons.get(index).click();
     }
 
+    /**
+     * Clicks the Remove button of the product selected by product name.
+     * @param name Name of the product on the list.
+     */
     public void removeProductByName(String name) {
         for ( int i = 0; i < productNames.size(); i++ ) {
             if ( productNames.get(i).getText().equals(name) ) {
@@ -68,11 +80,21 @@ public class CartPage {
         }
     }
 
+    /**
+     * Clicks the product link on the cart page, redirecting user to the Product page.
+     * @param index Number of the product on the list, starts w/ 0.
+     * @return Product page object.
+     */
     public ProductPage selectProductByNumber(int index) {
         productNameLinks.get(index).click();
         return new ProductPage(driver);
     }
 
+    /**
+     * Clicks the product link on the cart page, redirecting user to the Product page.
+     * @param name Name of the product on the list.
+     * @return Product page object.
+     */
     public ProductPage selectProductByName(String name) {
         for ( int i = 0; i < productNames.size(); i++ ) {
             if ( productNames.get(i).getText().equals(name) ) {
@@ -82,10 +104,20 @@ public class CartPage {
         return new ProductPage(driver);
     }
 
+    /**
+     * Returns the price of the product selected by index.
+     * @param index Number of the product on the list, starts w/ 0.
+     * @return Product price.
+     */
     public String getProductPriceByNumber(int index) {
         return productPrices.get(index).getText();
     }
 
+    /**
+     * Returns the price of the product selected by product name.
+     * @param name Name of the product on the list.
+     * @return Product price.
+     */
     public String getProductPriceByName(String name) {
         for ( int i = 0; i < productNames.size(); i++ ) {
             if ( productNames.get(i).getText().equals(name) ) {
@@ -95,10 +127,20 @@ public class CartPage {
         return null;
     }
 
+    /**
+     * Returns the total price of the product selected by index.
+     * @param index Number of the product on the list, starts w/ 0.
+     * @return Product total.
+     */
     public String getProductTotalByNumber(int index) {
         return productTotals.get(index).getText();
     }
 
+    /**
+     * Returns the total price of the product selected by product name.
+     * @param name Name of the product on the list.
+     * @return Product total.
+     */
     public String getProductTotalByName(String name) {
         for ( int i = 0; i < productNames.size(); i++ ) {
             if ( productNames.get(i).getText().equals(name) ) {
@@ -108,10 +150,20 @@ public class CartPage {
         return null;
     }
 
+    /**
+     * Sets the quantity of the product selected by index.
+     * @param index Number of the product on the list, starts w/ 0.
+     * @param quantity Quantity to set.
+     */
     public void setProductQuantityByNumber(int index, String quantity) {
         productQuantities.get(index).findElement(priceInput).sendKeys(quantity);
     }
 
+    /**
+     * Sets the quantity of the product selected by product name.
+     * @param name Name of the product on the list.
+     * @param quantity Quantity to set.
+     */
     public void setProductQuantityByName(String name, String quantity) {
         for ( int i = 0; i < productNames.size(); i++ ) {
             if ( productNames.get(i).getText().equals(name) ) {
@@ -121,28 +173,52 @@ public class CartPage {
         }
     }
 
+    /**
+     * Sets the coupon code field on the page.
+     * @param coupon Text containing the coupon.
+     */
     public void setCouponCode(String coupon) {
         couponCode.sendKeys(coupon);
     }
 
+    /**
+     * Clicks the Apply Coupon button on the page.
+     * @return Cart page object.
+     */
     public CartPage clickApplyCouponCode() {
         applyCouponButton.click();
         return this;
     }
 
+    /**
+     * Clicks the Update cart button on the page.
+     * @return Cart page object.
+     */
     public CartPage clickUpdateCart() {
         updateCartButton.click();
         return this;
     }
 
+    /**
+     * Returns the order Subtotal field value.
+     * @return Order subtotal value.
+     */
     public String getOrderSubtotal() {
         return orderSubtotal.getText();
     }
 
+    /**
+     * Returns the order Total field value.
+     * @return Order total value.
+     */
     public String getOrderTotal() {
         return orderTotal.getText();
     }
 
+    /**
+     * Clicks the Proceed to checkout button on the page.
+     * @return Checkout Page object.
+     */
     public CheckOutPage clickCheckoutButton() {
         checkoutButton.click();
         return new CheckOutPage(driver);
