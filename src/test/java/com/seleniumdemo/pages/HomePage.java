291ea99b28981158d60225a80aa61bdc9d9a058b
PapaHamf <jaroslaw.puchala@gmail.com>
2025-11-05 21:01:55 +0100

    Added docstrings

--- src/test/java/com/seleniumdemo/pages/HomePage.java
+++ src/test/java/com/seleniumdemo/pages/HomePage.java
@@ -39,26 +39,46 @@ public class HomePage {
     private List<WebElement> widgetItems;
     By itemName = By.xpath("//a[2]");
 
+    /**
+     * Class that holds the locators of the Home page and methods to get its webelements.
+     * @param driver
+     */
     public HomePage(WebDriver driver) {
         PageFactory.initElements(driver, this);
         this.driver = driver;
     }
 
+    /**
+     * Clicks the My Account link at the header of home page.
+     * @return MyAccount page object.
+     */
     public MyAccountPage clickMyAccount() {
         myAccountLink.click();
         return new MyAccountPage(driver);
     }
 
+    /**
+     * Clicks the Cart link at the header of home page.
+     * @return Cart page object.
+     */
     public CartPage clickCart() {
         cartLink.click();
         return new CartPage(driver);
     }
 
+    /**
+     * Clicks the Shop link at the header of home page.
+     * @return Shop page object.
+     */
     public ShopPage clickShop() {
         shopLink.click();
         return new ShopPage(driver);
     }
 
+    /**
+     * Clicks the Search button at the header of home page.
+     * @return Home page object.
+     */
     public HomePage clickSearchButton() {
         searchBarButton
                 .stream()
@@ -68,6 +88,10 @@ public class HomePage {
         return this;
     }
 
+    /**
+     * Clicks the Cart icon at the header of home page.
+     * @return Home page object.
+     */
     public HomePage clickCartIcon() {
         cartIcon
                 .stream()
@@ -77,6 +101,10 @@ public class HomePage {
         return this;
     }
 
+    /**
+     * Returns the interactive cart icon used to open the cart widget.
+     * @return Webelement of cart icon.
+     */
     public WebElement getInteractiveCartIcon() {
         return cartIcon
                 .stream()
@@ -85,28 +113,48 @@ public class HomePage {
                 .get();
     }
 
+    /**
+     * Displays the cart widget on the home page.
+     * @return Home page object.
+     */
     public HomePage displayCartWidget() {
         Actions action = new Actions(driver);
         action.moveToElement(getInteractiveCartIcon()).perform();
         return this;
     }
 
+    /**
+     * Returns the cart total amount for all of the products.
+     * @return Order total value.
+     */
     public String getCartWidgetTotal() {
         displayCartWidget();
         return productsTotal.getText();
     }
 
+    /**
+     * Clicks the View cart button inside the widget.
+     * @return Cart page object.
+     */
     public CartPage clickViewCartButton() {
         displayCartWidget();
         viewCartButton.click();
         return new CartPage(driver);
     }
 
+    /**
+     * Clicks the remove icon inside the widget for the product selected by index.
+     * @param index Number of the product on the list, starts w/ 0.
+     */
     public void removeProductByNumber(int index) {
         displayCartWidget();
         removeIcons.get(index).click();
     }
 
+    /**
+     * Clicks the remove icon inside the widget for the product selected by product name.
+     * @param name Name of the product on the list.
+     */
     public void removeProductByName(String name) {
         displayCartWidget();
         for ( int i = 0; i < widgetItems.size(); i++ ) {
@@ -117,11 +165,19 @@ public class HomePage {
         }
     }
 
+    /**
+     * Clicks the Checkout button inside the widget.
+     * @return Checkout page object.
+     */
     public CheckOutPage clickCheckoutButton() {
         checkOutButton.click();
         return new CheckOutPage(driver);
     }
 
+    /**
+     * Clicks the random post published on the home page.
+     * @return Post page object.
+     */
     public PostPage clickRandomArticle() {
         Random random = new Random();
         driver
