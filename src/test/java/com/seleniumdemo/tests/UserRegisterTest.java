0a7f50eb841fb1723d3f6b107fc5687017435cfd
PapaHamf <jaroslaw.puchala@gmail.com>
2025-11-07 19:38:13 +0100

    TCs 1-3

---
+++ src/test/java/com/seleniumdemo/tests/UserRegisterTest.java
@@ -0,0 +1,56 @@
+package com.seleniumdemo.tests;
+
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.Status;
+import com.seleniumdemo.pages.HomePage;
+import com.seleniumdemo.pages.MyAccountPage;
+import com.seleniumdemo.utils.TestDataProvider;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class UserRegisterTest extends BaseTest {
+
+    private static final Logger logger = LogManager.getLogger();
+    private static final TestDataProvider dataProvider = new TestDataProvider();
+
+    @Test()
+    public void registerUserWithoutEmail() {
+        ExtentTest test = extentReports.createTest("Register user without email adddress");
+        HomePage homePage = new HomePage(driver);
+        logger.info("Entering the My Account page");
+        test.log(Status.PASS, "Entering the My Account page");
+        MyAccountPage myAccountPage = homePage.clickMyAccount();
+        myAccountPage.enterRegisterPassword(dataProvider.generateCorrectPassword());
+        myAccountPage.clickRegisterButton();
+        Assert.assertEquals(myAccountPage.getErrorMessage(), MyAccountPage.INVALID_EMAIL);
+    }
+
+    @Test()
+    public void registerUserWithoutPassword() {
+        ExtentTest test = extentReports.createTest("Register user without password");
+        HomePage homePage = new HomePage(driver);
+        logger.info("Entering the My Account page");
+        test.log(Status.PASS, "Entering the My Account page");
+        MyAccountPage myAccountPage = homePage.clickMyAccount();
+        myAccountPage.enterRegisterEmail(dataProvider.faker.internet().emailAddress());
+        myAccountPage.clickRegisterButton();
+        Assert.assertEquals(myAccountPage.getErrorMessage(), MyAccountPage.INVALID_PASSWORD);
+    }
+
+    @Test()
+    public void registerUserWithInvalidEmail() {
+        ExtentTest test = extentReports.createTest("Register user with invalid email address");
+        HomePage homePage = new HomePage(driver);
+        logger.info("Entering the My Account page");
+        test.log(Status.PASS, "Entering the My Account page");
+        MyAccountPage myAccountPage = homePage.clickMyAccount();
+        String invalidEmail = dataProvider.faker.internet().emailAddress().replace("@", "");
+        myAccountPage.enterRegisterEmail(invalidEmail);
+        myAccountPage.enterRegisterPassword(dataProvider.generateCorrectPassword());
+        myAccountPage.clickRegisterButton();
+        Assert.assertEquals(myAccountPage.getTooltipErrorMessage(),
+                MyAccountPage.EMAIL_ADDR_WITHOUT_SIGN.replace("xxx", invalidEmail));
+    }
+}
