31ec9f47348ec98cb717e7320cc3427aa4627357
PapaHamf <jaroslaw.puchala@gmail.com>
2025-11-08 22:07:56 +0100

    Added TC 7 and logger lines

--- src/test/java/com/seleniumdemo/tests/UserRegisterTest.java
+++ src/test/java/com/seleniumdemo/tests/UserRegisterTest.java
@@ -4,6 +4,7 @@ import com.aventstack.extentreports.ExtentTest;
 import com.aventstack.extentreports.Status;
 import com.seleniumdemo.pages.HomePage;
 import com.seleniumdemo.pages.MyAccountPage;
+import com.seleniumdemo.pages.UserDashboardPage;
 import com.seleniumdemo.utils.TestDataProvider;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -22,7 +23,12 @@ public class UserRegisterTest extends BaseTest {
         logger.info("Entering the My Account page");
         test.log(Status.PASS, "Entering the My Account page");
         MyAccountPage myAccountPage = homePage.clickMyAccount();
-        myAccountPage.enterRegisterPassword(dataProvider.generateCorrectPassword());
+        String password = dataProvider.generateCorrectPassword();
+        logger.info("Entering password: " + password);
+        test.log(Status.PASS, "Entering password: " + password);
+        myAccountPage.enterRegisterPassword(password);
+        logger.info("Clicking the Register button");
+        test.log(Status.PASS, "Clicking the Register button");
         myAccountPage.clickRegisterButton();
         Assert.assertEquals(myAccountPage.getErrorMessage(), MyAccountPage.INVALID_EMAIL);
     }
@@ -34,7 +40,12 @@ public class UserRegisterTest extends BaseTest {
         logger.info("Entering the My Account page");
         test.log(Status.PASS, "Entering the My Account page");
         MyAccountPage myAccountPage = homePage.clickMyAccount();
-        myAccountPage.enterRegisterEmail(dataProvider.faker.internet().emailAddress());
+        String email = dataProvider.faker.internet().emailAddress();
+        logger.info("Entering email address: " + email);
+        test.log(Status.PASS, "Entering email address: " + email);
+        myAccountPage.enterRegisterEmail(email);
+        logger.info("Clicking the Register button");
+        test.log(Status.PASS, "Clicking the Register button");
         myAccountPage.clickRegisterButton();
         Assert.assertEquals(myAccountPage.getErrorMessage(), MyAccountPage.INVALID_PASSWORD);
     }
@@ -47,8 +58,15 @@ public class UserRegisterTest extends BaseTest {
         test.log(Status.PASS, "Entering the My Account page");
         MyAccountPage myAccountPage = homePage.clickMyAccount();
         String invalidEmail = dataProvider.faker.internet().emailAddress().replace("@", "");
+        logger.info("Entering email address: " + invalidEmail);
+        test.log(Status.PASS, "Entering email address: " + invalidEmail);
         myAccountPage.enterRegisterEmail(invalidEmail);
-        myAccountPage.enterRegisterPassword(dataProvider.generateCorrectPassword());
+        String password = dataProvider.generateCorrectPassword();
+        logger.info("Entering password: " + password);
+        test.log(Status.PASS, "Entering password: " + password);
+        myAccountPage.enterRegisterPassword(password);
+        logger.info("Clicking the Register button");
+        test.log(Status.PASS, "Clicking the Register button");
         myAccountPage.clickRegisterButton();
         Assert.assertEquals(myAccountPage.getTooltipErrorMessage(),
                 MyAccountPage.EMAIL_ADDR_WITHOUT_SIGN.replace("xxx", invalidEmail));
@@ -62,8 +80,16 @@ public class UserRegisterTest extends BaseTest {
         logger.info("Entering the My Account page");
         test.log(Status.PASS, "Entering the My Account page");
         MyAccountPage myAccountPage = homePage.clickMyAccount();
-        myAccountPage.enterRegisterEmail("@");
-        myAccountPage.enterRegisterPassword(dataProvider.generateCorrectPassword());
+        String email = "@";
+        logger.info("Entering email address: " + email);
+        test.log(Status.PASS, "Entering email address: " + email);
+        myAccountPage.enterRegisterEmail(email);
+        String password = dataProvider.generateCorrectPassword();
+        logger.info("Entering password: " + password);
+        test.log(Status.PASS, "Entering password: " + password);
+        myAccountPage.enterRegisterPassword(password);
+        logger.info("Clicking the Register button");
+        test.log(Status.PASS, "Clicking the Register button");
         myAccountPage.clickRegisterButton();
         Assert.assertEquals(myAccountPage.getTooltipErrorMessage(), MyAccountPage.EMAIL_ADDR_WITH_SIGN_ONLY);
     }
@@ -75,8 +101,14 @@ public class UserRegisterTest extends BaseTest {
         logger.info("Entering the My Account page");
         test.log(Status.PASS, "Entering the My Account page");
         MyAccountPage myAccountPage = homePage.clickMyAccount();
-        myAccountPage.enterRegisterEmail(dataProvider.faker.internet().emailAddress());
-        myAccountPage.enterRegisterPassword(dataProvider.generatePlainPasswordShorterThan12Chars());
+        String email = dataProvider.faker.internet().emailAddress();
+        logger.info("Entering email address: " + email);
+        test.log(Status.PASS, "Entering email address: " + email);
+        myAccountPage.enterRegisterEmail(email);
+        String password = dataProvider.generatePlainPasswordShorterThan8Chars();
+        logger.info("Entering password: " + password);
+        test.log(Status.PASS, "Entering password: " + password);
+        myAccountPage.enterRegisterPassword(password);
         Assert.assertEquals(myAccountPage.getPasswordStrengthMessage(), MyAccountPage.PASSWORD_WEAK);
     }
 
@@ -88,8 +120,34 @@ public class UserRegisterTest extends BaseTest {
         logger.info("Entering the My Account page");
         test.log(Status.PASS, "Entering the My Account page");
         MyAccountPage myAccountPage = homePage.clickMyAccount();
-        myAccountPage.enterRegisterEmail(dataProvider.faker.internet().emailAddress());
-        myAccountPage.enterRegisterPassword(dataProvider.generatePasswordWithoutOtherCharacters());
+        String email = dataProvider.faker.internet().emailAddress();
+        logger.info("Entering email address: " + email);
+        test.log(Status.PASS, "Entering email address: " + email);
+        myAccountPage.enterRegisterEmail(email);
+        String password = dataProvider.generatePasswordWithoutOtherCharacters();
+        logger.info("Entering password: " + password);
+        test.log(Status.PASS, "Entering password: " + password);
+        myAccountPage.enterRegisterPassword(password);
         Assert.assertEquals(myAccountPage.getPasswordStrengthMessage(), MyAccountPage.PASSWORD_VERY_WEAK);
+    }
+
+    @Test()
+    public void registerUserWithValidData() {
+        ExtentTest test = extentReports.createTest("Register user with password without" +
+                " upper case letters, numbers and special characters");
+        HomePage homePage = new HomePage(driver);
+        logger.info("Entering the My Account page");
+        test.log(Status.PASS, "Entering the My Account page");
+        MyAccountPage myAccountPage = homePage.clickMyAccount();
+        String email = dataProvider.faker.internet().emailAddress();
+        logger.info("Entering email address: " + email);
+        test.log(Status.PASS, "Entering email address: " + email);
+        myAccountPage.enterRegisterEmail(email);
+        String password = dataProvider.generateCorrectPassword();
+        logger.info("Entering password: " + password);
+        test.log(Status.PASS, "Entering password: " + password);
+        myAccountPage.enterRegisterPassword(password);
+        UserDashboardPage userDashboardPage = myAccountPage.clickRegisterButton();
+        Assert.assertTrue(userDashboardPage.getDashboardLink().isDisplayed());
     }
 }
